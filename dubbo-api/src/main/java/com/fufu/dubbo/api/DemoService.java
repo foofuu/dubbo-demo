855e23e9d7773e7ef8a617ed5084f0e1f3e7ea10
周隽夫 <zhoujunfuv5@163.com>
2018-08-15 14:02:36 +0800

    first commit

---
+++ dubbo-api/src/main/java/com/fufu/dubbo/api/DemoService.java
@@ -0,0 +1,8 @@
+package com.fufu.dubbo.api;
+
+/**
+ * Created by zhoujunfu on 2018/5/21.
+ */
+public interface DemoService {
+    String sayHello(String name);
+}
---
+++ dubbo-provider/src/main/java/com/fufu/dubbo/provider/service/DemoServiceImpl.java
@@ -0,0 +1,14 @@
+package com.fufu.dubbo.provider.service;
+
+
+import com.fufu.dubbo.api.DemoService;
+
+/**
+ * Created by zhoujunfu on 2018/5/21.
+ */
+public class DemoServiceImpl implements DemoService {
+    public String sayHello(String name) {
+        return "Hello " + name;
+    }
+
+}
