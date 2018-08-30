edcb3650a1f370e59550451337d71092e7892bba
fufu <zhoujunfuv5@163.com>
2018-08-30 19:44:41 +0800

    demo

--- dubbo-consumer/src/main/java/com/fufu/consumer/controller/DemoController.java
+++ dubbo-consumer/src/main/java/com/fufu/consumer/controller/DemoController.java
@@ -20,13 +20,13 @@ public class DemoController {
 
     private static Logger logger = LoggerFactory.getLogger(DemoController.class);
 
-    /*@Resource
-    DemoService demoService;*/
+    @Resource
+    DemoService demoService;
 
     @RequestMapping(value ="/test",method={RequestMethod.POST,RequestMethod.GET})
     @ResponseBody
-    public String testCategoryAccuracy(@RequestParam String name) {
-        /*return demoService.sayHello(name);*/
-        return name;
+    public String test(@RequestParam String name) {
+        logger.info("test name, " + name);
+        return demoService.sayHello(name);
     }
 }
